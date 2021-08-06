3d967cb5301fdbf66b1109f222729a84fb983516
tasinislamkhan <tasinislamkhan@gmail.com>
2021-08-06 15:15:01 +0600

    firstUpload

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+$(function() {
+    ' use strict ' ;
+    $(".banner_slider_container").slick({
+        fade: true,
+        arrows:false,
+        autoplay:true,
+        speed:1000,
+        autoplayspeed:2000,
+    });
+})
